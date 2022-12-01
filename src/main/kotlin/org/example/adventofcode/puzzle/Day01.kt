2416e1d10041b3f2656cd138496a798139f432ce
wcchristian <christian.andersen@datasite.com>
2022-12-01 14:58:31 -0600

    Day 1 solution

--- src/main/kotlin/org/example/adventofcode/puzzle/Day01.kt
+++ src/main/kotlin/org/example/adventofcode/puzzle/Day01.kt
@@ -1,10 +1,42 @@
 package org.example.adventofcode.puzzle
 
-fun print() {
-    print("yo")
+import org.example.adventofcode.util.FileLoader
+
+fun part1(filePath: String): Int {
+    var elfList = getElfList(filePath)
+    elfList = elfList.sortedDescending()
+    return elfList.first()
 }
 
+fun part2(filePath: String): Int {
+    var elfList = getElfList(filePath)
+    elfList = elfList.sortedDescending()
+    return elfList[0] + elfList[1] + elfList[2]
+}
+
+fun getElfList(filePath: String): List<Int> {
+    val fileLines = FileLoader.loadFromFile<String>(filePath)
+
+    var currentCount = 0
+    val elves = ArrayList<Int>()
+    for (line in fileLines) {
+        if (line == "") {
+            elves.add(currentCount)
+            currentCount = 0
+        } else {
+            currentCount += line.toInt()
+        }
+    }
+
+    // last line
+    elves.add(currentCount)
+
+    return elves
+}
 
 fun main() {
-    print()
+    println("Part 1 example solution is: ${part1("/day01_example.txt")}")
+    println("Part 1 main solution is: ${part1("/day01.txt")}")
+    println("Part 2 example solution is: ${part2("/day01_example.txt")}")
+    println("Part 2 main solution is: ${part2("/day01.txt")}")
 }
